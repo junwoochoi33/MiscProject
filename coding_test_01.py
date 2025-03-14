28fde7e915da6f3e81d18ad3bff4302786b880ad
jwchoi <jwchoi@wisenut.co.kr>
2025-03-14 09:21:55 +0900

    fix: change coding_test_01's input

--- coding_test_01.py
+++ coding_test_01.py
@@ -16,7 +16,6 @@ def coding_test_01():
     조건:
     keyword는 대소문자를 구분하지 않으며, 단어의 앞뒤 공백은 무시합니다.
     quotes에 있는 각 명언은 공백을 기준으로 단어들이 구분됩니다.
-    키워드가 특수문자일 경우 검색되지 않아야 합니다. (예: '-')
     명언은 입력된 순서대로 출력되어야 합니다.
 
     입력 설명:
@@ -24,18 +23,21 @@ def coding_test_01():
     이후 정해진 개수만큼의 명언과 검색어가 주어집니다.
 
     입력 예시1:
-    5 1
+    5 2
     자신의 마음을 비우고, 새로운 것을 받아들여라. - 라오쯔
     자신의 능력을 믿어라. - 나폴레옹 힐
     자신의 선택을 존중하라. - 롭 위머
-    어떤 일을 시작할 때 가장 중요한 것은 그것을 끝까지 이어가는 것이다. - 윈스턴 처칠
+    어떤 일을 시작할 때 가장 중요한 것은 그것을 끝까지 이어가는 것이다. - 윈스턴 처칠 dfdf
     자신을 믿어라. 그게 시작이다. - 존 웨인
     자신의
+    믿어라.
 
     출력 예시1:
     자신의 마음을 비우고, 새로운 것을 받아들여라. - 라오쯔
     자신의 능력을 믿어라. - 나폴레옹 힐
     자신의 선택을 존중하라. - 롭 위머
+    자신의 능력을 믿어라. - 나폴레옹 힐
+    자신을 믿어라. 그게 시작이다. - 존 웨인
 
     입력 예시2:
     5 1
@@ -59,16 +61,19 @@ def coding_test_01():
         quote = input().strip()
         quotes.append(quote)
         for token in quote.split():
-            if token.isalnum():
-                token = token.lower()
-                index[token].add(docid)
+            token = token.lower()
+            index[token].add(docid)
 
-    keyword = input().strip().lower()
-    if keyword in index:
-        for docid in index[keyword]:
-            print(quotes[docid])
-    else:
-        print(f"키워드 '{keyword}'를 포함하는 명언이 없습니다.")
+    keywords = [input().strip() for _ in range(m)]
+
+    for keyword in keywords:
+        key = keyword.lower()
+        if key in index:
+            for docid in index[key]:
+                print(quotes[docid])
+        else:
+            print(f"키워드 '{keyword}'를 포함하는 명언이 없습니다.")
+
 
 if __name__ == '__main__':
     coding_test_01()
