7cb693d481f35d464ddbd6484a89e3e9902a4420
jwchoi <jwchoi@wisenut.co.kr>
2025-04-11 14:17:26 +0900

    feat: add assembly_line_scheduling.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from selection_sort import selection_sort
+from assembly_line_scheduling import assembly_line_scheduling
 
 if __name__ == '__main__':
-    arr = [3, 31, 48, 73, 8, 11, 20, 29, 65, 15]
-    print(selection_sort(arr))
+    a = [[7, 9, 3, 4, 8, 4],
+         [8, 5, 6, 4, 5, 7]]
+    t = [[2, 1, 1, 3, 4],
+         [2, 1, 2, 2, 1]]
+    e = [2, 4]
+    x = [3, 2]
+
+    time, path = assembly_line_scheduling(a, t, e, x)
+    print(f"최소 시간: {time}")
+    print(f"경로: {path}")
